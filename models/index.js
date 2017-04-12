cc3a2c98f1f38620586b3303d90dd3d3d90287d2
kwadrnt <kevinctse@gmail.com>
2017-04-11 23:54:27 -0700

    sprint 1 almost complete

--- models/index.js
+++ models/index.js
@@ -1,2 +1,17 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+
+// models/index.js
+module.exports.Book = require("./book.js");
+
+
+// // models/index.js
+// var mongoose = require("mongoose");
+// mongoose.connect("mongodb://localhost/book-app");   
+// // the mongoose.connect line above  needs to happen exactly once in your code
+//     // move it from book.js to index.js  :)
+
+// module.exports.Gargoyle = require("./gargoyle.js");
+// module.exports.Goblin = require("./goblin.js");
+// module.exports.Gnome = require("./gnome.js");
