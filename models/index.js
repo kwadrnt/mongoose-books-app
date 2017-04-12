e18ae637c8839e96634331c96359cfde6d8f9432
kwadrnt <kevinctse@gmail.com>
2017-04-12 16:23:41 -0700

    script 2 complete

--- models/index.js
+++ models/index.js
@@ -1,17 +1,10 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
 
+// Use native JavaScript promises to avoid warning
+mongoose.Promise = global.Promise;
 
-// models/index.js
 module.exports.Book = require("./book.js");
+module.exports.Author = require("./author.js");
 
 
-// // models/index.js
-// var mongoose = require("mongoose");
-// mongoose.connect("mongodb://localhost/book-app");   
-// // the mongoose.connect line above  needs to happen exactly once in your code
-//     // move it from book.js to index.js  :)
-
-// module.exports.Gargoyle = require("./gargoyle.js");
-// module.exports.Goblin = require("./goblin.js");
-// module.exports.Gnome = require("./gnome.js");
